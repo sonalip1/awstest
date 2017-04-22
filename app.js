6ca5a4a4d172062ed4cedf4de1481e2a1ccbfcdf
Manikandan <manikandan.citeee@gmail.com>
2017-04-22 15:48:30 +0530

    Third set checkin

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var express  = require('express');
 var app      = express();     
+var AWS = require('aws-sdk');
 
 app.use(express.static(__dirname + '/public')); 
+
+var dynamoDB = new AWS.DynamoDB.DocumentClient({
+	accessKeyId: 'AKIAIKXAMZQUFK7UQDEQ',
+	secretAccessKey: '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',
+	region: 'ap-northeast-1'
+});
+
+app.get('/name',function(req,res){
+	dynamoDB.get({
+					TableName: 'AWS_Test',
+					Key: {'ID':3, 'Name': "Sonali"}
+
+				}, function(err, docs){
+					console.log('err ', err, 'docs ',docs);
+					res.json(docs);
+				})
+});
 
 app.listen(80);
 console.log("App listening on port 80");
